1cbae6e6012e6bda50cd54bb3f4f993e2bdddf0d
Aaradhya1702 <aaradhyajain961@gmail.com>
2025-04-26 22:23:07 +0530

    photo added

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,6 +7,20 @@ const About = () => {
       className="rich-text-block p-4 bg-white border-t-2 border-black w-full min-h-[50vh]"
     >
       <div className="w-[75%] mx-auto h-full">
+        <div className="w-max bg-white border-4 border-black shadow-[4px_4px_0_0_black] flex-shrink-0">
+          <div className="flex justify-between items-center bg-gray-300 px-4 py-2 border-b-2 border-black">
+            <div className="flex gap-2">
+              <span className="w-3 h-3 bg-black rounded-full"></span>
+              <span className="w-3 h-3 bg-black rounded-full"></span>
+              <span className="w-3 h-3 bg-black rounded-full"></span>
+            </div>
+          </div>
+
+          <div className="p-4">
+            <img alt="me" className="w-[200px]" src="/me.jpg" />
+          </div>
+        </div>
+
         <h1 className="text-6xl my-8 font-bold">About Me</h1>
         <p>
           Hey! I’m Aaradhya Jain, a full-stack web developer based in Jaipur,
